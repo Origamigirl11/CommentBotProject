49d8ebfac9cf41f52e8cdbfe237b86395b838777
Origamigirl11 <origamigirl11@gmail.com>
2019-12-05 16:31:30 -0600

    Getting familier
    played with some commands to experiment and get familiar with data structure.

--- commentGetter.py
+++ commentGetter.py
@@ -1,20 +1,51 @@
 import praw
 
-bot = praw.Reddit(user_agent='MySimpleBot v0.1',
-                  client_id='abcdefGHIJKLO123--',
-                  client_secret='12345678901abcdefgh-_',
-                  username='demo_user',
-                  password='demo_password')
+# bot = praw.Reddit(user_agent='MySimpleBot v0.1',
+#                   client_id='abcdefGHIJKLO123--',
+#                   client_secret='12345678901abcdefgh-_',
+#                   username='demo_user',
+#                   password='demo_password')
 
-subreddit = bot.subreddit('AbortionDebate')
 
-comments = subreddit.stream.comments()
+bot.read_only = True
+print(bot.auth.scopes())
 
-for comment in comments:
-    text = comment.body # Fetch body
-    author = comment.author # Fetch author
-    if 'strawman' in text.lower():
-        # Generate a message
-        message = "Message - /{1}, u/{0} ?".format(author, len(text))
+subreddit = bot.subreddit("AbortionDebate")
 
-        print(message) # Send message
+submission = bot.submission(url='https://www.reddit.com/r/funny/comments/3g1jfi/buttons/')
+
+# for top_level_comment in submission.comments:
+#     try:
+#         print(top_level_comment.body)
+#     except AttributeError:
+#         print('no body - first level')
+#     for second_level_comment in submission.comments:
+#         try:
+#             print('\t', second_level_comment.body)
+#         except AttributeError:
+#             print('\tno body - second level')
+#
+submission.comments.replace_more(limit=None)
+comment_queue = submission.comments[:]  # Seed with top-level
+for comment in comment_queue:
+    print(comment.body)
+    for replies in comment.replies:
+        print('\t', replies.body)
+
+#
+# submission.comments.replace_more(limit=None)
+# for comment in submission.comments.list():
+#     print(comment.body)
+
+# comments = subreddit.stream.comments()
+#
+# #comments = subreddit.get_comments(limit=None)
+#
+# for comment in comments:
+#     text = comment.body # Fetch body
+#     author = comment.author # Fetch author
+#     if 'man' in text.lower():
+#         # Generate a message
+#         message = "Message - /{1}, u/{0} ?".format(author, len(text))
+#
+#         print(message) # Send message
